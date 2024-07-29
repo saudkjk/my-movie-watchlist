873092e9a46cf2b301f12efd9fc7c2e141280797
SAOUD ALDOWAISH <u1275778@umail.utah.edu>
2024-07-29 17:42:09 -0600

    remove unnecessary elements

--- src/components/ui/ImageHover.tsx
+++ src/components/ui/ImageHover.tsx
@@ -7,44 +7,35 @@ export const ImageHover = ({
   imageUrl,
   children,
   childrenClassName,
-  imageClassName,
   className,
 }: {
   imageUrl: string;
   children: React.ReactNode | string;
   childrenClassName?: string;
-  imageClassName?: string;
   className?: string;
 }) => {
   return (
     <div
       className={cn(
-        "h-full w-full inset-0 bg-transparent rounded-lg overflow-hidden group",
+        "relative h-full w-full inset-0 bg-transparent rounded-lg overflow-hidden group",
         className
       )}
     >
-      <div className="relative h-full w-full group-hover:block">
-        <div className="hidden group-hover:block absolute inset-0 w-full h-full bg-black/40 z-10 transition duration-500" />
-        <div className="h-full w-full relative bg-gray-50 dark:bg-black transition duration-500 ease-out transform group-hover:scale-[1.10]">
-          <div className="relative h-full w-full">
-            <Image
-              alt="image"
-              className={`h-full w-full ${imageClassName}`}
-              fill
-              src={imageUrl}
-              sizes="(max-width: 480px) 35vw, (max-width: 768px) 30vw, (max-width: 992px) 25vw, (max-width: 1200px) 20vw, (max-width: 1600px) 15vw, 10vw"
-              quality={80}
-            />
-          </div>
-        </div>
-        <div
-          className={cn(
-            "text-white absolute bottom-0 left-4 z-40 transition duration-500 ease-out opacity-0 group-hover:opacity-100",
-            childrenClassName
-          )}
-        >
-          {children}
-        </div>
+      <Image
+        alt="image"
+        className={`transition duration-500 ease-in-out transform group-hover:scale-[1.10]  group-hover:brightness-75`}
+        fill
+        src={imageUrl}
+        sizes="(max-width: 480px) 35vw, (max-width: 768px) 30vw, (max-width: 992px) 25vw, (max-width: 1200px) 20vw, (max-width: 1600px) 15vw, 10vw"
+        quality={80}
+      />
+      <div
+        className={cn(
+          "text-white absolute bottom-0 left-4 z-40 transition duration-500 ease-out opacity-0 group-hover:opacity-100",
+          childrenClassName
+        )}
+      >
+        {children}
       </div>
     </div>
   );
