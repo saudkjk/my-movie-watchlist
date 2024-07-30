e9eb43c413dd16e54839a199f51386c7820d4f16
SAOUD ALDOWAISH <u1275778@umail.utah.edu>
2024-07-30 06:35:58 -0600

    removed console.log

--- src/components/AnimatedBorder.tsx
+++ src/components/AnimatedBorder.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default async function AnimatedBorder({ children }: any) {
-  console.log("consel?");
   return (
     <div className="animate-gradient-animation rounded-lg bg-gradient-to-r from-pink-500 via-blue-500 to-indigo-900 bg-[length:200%_200%] p-0.5">
       {children}
